fec879df5776889e199e4739acbce1b5596ef818
LeLoader <leloaderpro@gmail.com>
2026-02-11 16:27:54 +0100

    Dog controller, and trampling anim

--- Assets/Resources/Data/Actions/Scripts/Action/ACT_TrampleSapling.cs
+++ Assets/Resources/Data/Actions/Scripts/Action/ACT_TrampleSapling.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class ACT_TrampleSapling : ActionBase
@@ -24,7 +26,19 @@ public class ACT_TrampleSapling : ActionBase
         base.OnActionDestinationReached();
 
         Destroy(targetSapling);
-        ValidationAction(EReturnState.SUCCEEDED);
+        _behaviorController.CallTriggerAnimation("stomp");
+
+        Action action = () => { ValidationAction(EReturnState.SUCCEEDED); };
+        StartCoroutine(WaitForEndOfAnimation(action, _behaviorController));
+    }
+
+    public IEnumerator WaitForEndOfAnimation(Action action, BehaviorController controller)
+    {
+        while (controller.GetAnimator().GetCurrentAnimatorStateInfo(0).normalizedTime <= 1)
+        {
+            yield return null;
+        }
+        action?.Invoke();
     }
 
     public override bool StopAction(EStopActionReason reason)
